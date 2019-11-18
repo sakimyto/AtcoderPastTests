b5d03ce97d55d69d573a2477ae0c2daf8b8bf011
sakimyto <yuto.sakimae@gmail.com>
2019-11-18 23:50:41 +0900

    Add ABC145A~ABC145E.

--- v-3.4.3/ABC-E/ABC145E.py
+++ v-3.4.3/ABC-E/ABC145E.py
@@ -1,5 +1,72 @@
 def resolve():
     import numpy as np
     n, t = map(int, input().split())
-    lst = [[int(i) for j in input().split()] for _ in range(n)]
+    lst = np.full((t), -float("inf"))
+    lst[0] = 0
+    ans = 0
+    ab = [list(map(int, input().split())) for i in range(n)]
+    ab.sort()
+    for a, b in ab:
+        ans = max(ans, max(lst) + b)
+        if a < t:
+            lst[a:] = np.maximum(lst[a:], lst[:-a] + b)
+    ans = int(ans)
+    print(ans)
 
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """2 60
+10 10
+100 100"""
+        output = """110"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3 60
+10 10
+10 20
+10 30"""
+        output = """60"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """3 60
+30 10
+30 20
+30 30"""
+        output = """50"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """10 100
+15 23
+20 18
+13 17
+24 12
+18 29
+19 27
+23 21
+18 20
+27 15
+22 25"""
+        output = """145"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
