83dc7d08994b74e2f669064e7b2419e7d58dfc31
sakimyto <yuto.sakimae@gmail.com>
2020-03-09 00:23:16 +0900

    Fix ABC158C

--- ABC-C/ABC158C.py
+++ ABC-C/ABC158C.py
@@ -1,14 +1,17 @@
 def resolve():
     a, b = map(int, input().split())
-    for i in range(10000):
+    ans = -1
+    for i in range(1001):
         if int(i * 0.08) == a and int(i * 0.1) == b:
-            print(i)
-            exit()
-    print(-1)
+            ans = i
+            break
+    print(ans)
+
 
 import sys
 from io import StringIO
 import unittest
+
 
 class TestClass(unittest.TestCase):
     def assertIO(self, input, output):
@@ -19,18 +22,22 @@ class TestClass(unittest.TestCase):
         out = sys.stdout.read()[:-1]
         sys.stdout, sys.stdin = stdout, stdin
         self.assertEqual(out, output)
+
     def test_入力例_1(self):
         input = """2 2"""
         output = """25"""
         self.assertIO(input, output)
+
     def test_入力例_2(self):
         input = """8 10"""
         output = """100"""
         self.assertIO(input, output)
+
     def test_入力例_3(self):
         input = """19 99"""
         output = """-1"""
         self.assertIO(input, output)
+
 
 if __name__ == "__main__":
     unittest.main()
